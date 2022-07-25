d441053e8f01322f413dd9103893df0a2af535cb
ivanribeirodf <ivandf@gmail.com>
2022-07-25 20:50:58 -0300

    first commit

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,12 @@
+import { useRef, useEffect,useState } from 'react';
+import Image from 'next/Image';
+import {usePlayer } from '../../contexts/PlayerContext';
+
+import styles from './styles.module.scss';
+import {} from '../../utils/convertDurationToTimeString';
+
+export default function Player () {
+  return(
+    
+  )
+}
